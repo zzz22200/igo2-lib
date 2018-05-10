34dcf8e27f61358e49859d332343e11d9597ab93
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-05-10 14:09:31 -0400

    fix(login): show error only on component

--- src/lib/auth/auth-form/auth-intern.component.ts
+++ src/lib/auth/auth-form/auth-intern.component.ts
@@ -1,5 +1,9 @@
-import { Component, ChangeDetectionStrategy,
-  Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { AuthService } from '../shared/auth.service';
 
@@ -15,10 +19,7 @@ export class AuthInternComponent {
 
   @Output() onLogin: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(
-    public auth: AuthService,
-    fb: FormBuilder
-  ) {
+  constructor(public auth: AuthService, fb: FormBuilder) {
     this.form = fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -26,15 +27,15 @@ export class AuthInternComponent {
   }
 
   login(values: any) {
-    this.auth.login(values.username, values.password)
-      .subscribe(
-        () => {
-          this.onLogin.emit(true);
-        },
-        (error: any) => {
-          this.error = error.error.message;
-        }
-      );
+    this.auth.login(values.username, values.password).subscribe(
+      () => {
+        this.onLogin.emit(true);
+      },
+      (error: any) => {
+        this.error = error.error.message;
+        error.error.caught = true;
+      }
+    );
     return false;
   }
 
