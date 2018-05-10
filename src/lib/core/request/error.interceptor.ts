f7e9be67b9ab8074ed14746d9af6b8fe641ce5cd
Marc-André Barbeau <marc-andre.barbeau@msp.gouv.qc.ca>
2018-05-10 14:10:13 -0400

    fix(error): show error once

--- src/lib/core/request/error.interceptor.ts
+++ src/lib/core/request/error.interceptor.ts
@@ -1,6 +1,11 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent,
-  HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpEvent,
+  HttpErrorResponse
+} from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { catchError } from 'rxjs/operators/catchError';
@@ -12,21 +17,29 @@ import { LanguageService } from '../language/shared/language.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private messageService: MessageService,
+    private injector: Injector
+  ) {}
 
-  private httpError: HttpErrorResponse;
-
-  constructor(private messageService: MessageService,
-              private injector: Injector) {}
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let httpError: ErrorObservable;
     return next.handle(req).pipe(
-      catchError((error) => this.handleError(error, req)),
-      finalize(() => this.handleUncaughtError())
+      catchError(error => {
+        httpError = this.handleError(error, req);
+        return httpError;
+      }),
+      finalize(() => this.handleUncaughtError(httpError))
     );
   }
 
   private handleError(httpError: HttpErrorResponse, req: HttpRequest<any>) {
-    const msg = `${req.method} ${req.urlWithParams} ${httpError.status} (${httpError.statusText})`;
+    const msg = `${req.method} ${req.urlWithParams} ${httpError.status} (${
+      httpError.statusText
+    })`;
 
     if (httpError instanceof HttpErrorResponse) {
       const errorObj = httpError.error === 'object' ? httpError.error : {};
@@ -34,20 +47,21 @@ export class ErrorInterceptor implements HttpInterceptor {
       errorObj.caught = false;
       console.error(msg, '\n', errorObj.message, '\n\n', httpError);
 
-      this.httpError = new HttpErrorResponse({
-          error: errorObj,
-          headers: httpError.headers,
-          status: httpError.status,
-          statusText: httpError.statusText,
-          url: httpError.url
+      httpError = new HttpErrorResponse({
+        error: errorObj,
+        headers: httpError.headers,
+        status: httpError.status,
+        statusText: httpError.statusText,
+        url: httpError.url
       });
     }
 
-    return new ErrorObservable(this.httpError);
-  };
+    return new ErrorObservable(httpError);
+  }
 
-  private handleUncaughtError() {
-    if (this.httpError && !this.httpError.error.caught) {
+  private handleUncaughtError(httpError$: ErrorObservable) {
+    const httpError = httpError$.error;
+    if (httpError && !httpError.error.caught) {
       const translate = this.injector.get(LanguageService).translate;
       const message = translate.instant('igo.errors.uncaught.message');
       const title = translate.instant('igo.errors.uncaught.title');
